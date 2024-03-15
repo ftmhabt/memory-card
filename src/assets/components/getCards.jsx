bbbcbf693f2a65e2b95b0678e62e4141401e9303
ftmhabt <ftmh.a78@gmail.com>
2024-03-15 17:57:39 +0330

    Fix api fetching with use effect

--- src/assets/components/getCards.jsx
+++ src/assets/components/getCards.jsx
@@ -1,28 +1,27 @@
-export default function GetCards(){
-    let allCards=[];
-    const key='live_TcC4FpAsADtICHsK7oh588oZaCve8Gn99tzudM5JAuAfg8ImYjxpmUMTaxLLcg7V';
+import  { useState, useEffect } from 'react';
 
-    async function getCats() {
-        const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=100&api_key=${key}`,
-            { mode: 'cors' });
-        const json = await response.json();
-        return json;
-    }
+export default function GetCards() {
+    const [allCards, setAllCards] = useState([]);
 
-    async function fill(){
-
-        let catJson= await getCats();
-        
-        for (let i = 0; i < 100; i++) {
-            let cat={id:'',img:''};
-            cat.id=catJson[i].id;
-            cat.img=catJson[i].url;
-
-
-            allCards.push(cat);
+    useEffect(() => {
+        async function getCats() {
+            const response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10`,
+                { mode: 'cors' });
+            const json = await response.json();
+            return json;
         }
-    }
 
-    fill();
-    
+        async function fill(){
+            let catJson= await getCats();
+            let cards = [];
+            for (let i = 0; i < 10; i++) {
+                cards.push(catJson[i].url);
+            }
+            setAllCards(cards);
+        }
+
+        fill();
+    }, []);
+
+    return allCards;
 }
